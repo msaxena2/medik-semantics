c81f6de0db8b5e5b43e045029e5a160c6f35dced
Manasvi Saxena <msaxena2@illinois.edu>
2023-09-19 19:45:27 -0500

    medik_pyk/kompile.py:WIP on using pyk

--- medik-pyk/src/medik_pyk/__main__.py
+++ medik-pyk/src/medik_pyk/__main__.py
@@ -1,26 +1,42 @@
 from argparse import ArgumentParser
 from pyk.ktool.kompile import KompileArgs, LLVMKompile, LLVMKompileType
 
+from .kompile import KompileTarget
+
 import sys
 
 def create_arg_parser():
     parser = ArgumentParser(prog='kevm-pyk')
     command_parser = parser.add_subparsers(dest='command', required=True)
 
+    kompile_args = command_parser.add_parser(
+          'kompile'
+        , help='Kompile MediK Definition.'
+    )
+
+    kompile_args.add_argument(
+            dest='target'
+          , type=KompileTarget
+          , help='[llvm|llvm-mcheck|haskell]'
+    )
+
+    #kompile_args.add_argument('main_file', type=file_path, help='Path to file with main module.')
+    #kompile_args.add_argument('--target', type=KompileTarget, help='[llvm|haskell|haskell-standalone|foundry]')
+    #kompile_args.add_argument(
+    #    '-o', '--output-definition', type=Path, dest='output_dir', help='Path to write kompiled definition to.'
+    #)
+
+    krun_args = command_parser.add_parser(
+          'krun'
+        , help='Run MediK Programs.'
+    )
+
+    return parser
 
 def main():
     sys.setrecursionlimit(15000000)
-    print('hello poetry')
-    parser = create_argument_parser()
+    parser = create_arg_parser()
     args = parser.parse_args()
-    logging.basicConfig(level=_loglevel(args), format=_LOG_FORMAT)
-
-    executor_name = 'exec_' + args.command.lower().replace('-', '_')
-    if executor_name not in globals():
-        raise AssertionError(f'Unimplemented command: {args.command}')
-
-    execute = globals()[executor_name]
-    execute(**vars(args))
 
 if __name__ == '__main__':
     main()
