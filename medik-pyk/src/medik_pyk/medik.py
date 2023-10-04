e02f8726b60e79b83fb5dc01aa4984b5e1f41c75
Manasvi Saxena <msaxena2@illinois.edu>
2023-10-04 16:35:31 -0500

    medik.py,build.py:Move building functionality to standalone module

--- medik-pyk/src/medik_pyk/medik.py
+++ medik-pyk/src/medik_pyk/medik.py
@@ -1,78 +1,40 @@
 from __future__ import annotations
 
 import logging
-import shutil
 from argparse import ArgumentParser
-from enum import Enum
 from typing import TYPE_CHECKING
 
-from . import config
-from .kompile import KompileTarget, medik_kompile
+from pyk.cli.utils import file_path
+from pyk.ktool.krun import KRunOutput
+
+from .build import BuildTarget
 
 if TYPE_CHECKING:
-    from collections.abc import Mapping
     from pathlib import Path
-    from typing import Any, Final
+    from typing import Final
 
 
 _LOGGER: Final = logging.getLogger(__name__)
 _LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
 
-# ---------
-# K targets
-# ---------
+# ----------------
+# Running Programs
+# ----------------
 
 
-class BuildTarget(Enum):
-    LLVM = 'llvm'
-    LLVM_MCHECK = 'llvm-mcheck'
-    HASKELL = 'haskell'
-
-    @property
-    def path(self) -> Path:
-        return config.BUILD_DIR / self.value
-
-    def clean(self) -> Path:
-        shutil.rmtree(self.path, ignore_errors=True)
-        return self.path
-
-    def do_build(self) -> None:
-        _LOGGER.info(f'Building target {self.name}: {self.path}')
-        config.BUILD_DIR.mkdir(parents=True, exist_ok=True)
-        try:
-            medik_kompile(output_dir=self.path, **_TARGET_PARAMS[self])
-        except RuntimeError:
-            self.clean()
-            raise
-
-
-_TARGET_PARAMS: Final[Mapping[BuildTarget, Any]] = {
-    BuildTarget.LLVM: {
-        'target': KompileTarget.LLVM,
-        'main_file': config.MEDIK_DIR / 'medik.md',
-        'main_module': 'MEDIK',
-        'syntax_module': 'MEDIK-SYNTAX',
-    },
-    BuildTarget.LLVM_MCHECK: {
-        'target': KompileTarget.LLVM,
-        'main_file': config.MEDIK_DIR / 'medik.md',
-        'main_module': 'MEDIK',
-        'syntax_module': 'MEDIK-SYNTAX',
-    },
-    BuildTarget.HASKELL: {
-        'target': KompileTarget.HASKELL,
-        'main_file': config.MEDIK_DIR / 'medik.md',
-        'main_module': 'MEDIK',
-        'syntax_module': 'MEDIK-SYNTAX',
-    },
-}
+def exec_run(input_file: Path, output_mode: KRunOutput = KRunOutput.PRETTY) -> None:
+    if not BuildTarget.LLVM.path.exists():
+        raise RuntimeError(f'Target {str(BuildTarget.LLVM.value)} does not exist.')
 
 
 def _build_arg_parser() -> ArgumentParser:
-    parser = ArgumentParser(prog='medik-build')
+    parser = ArgumentParser(prog='medik')
     command_parser = parser.add_subparsers(dest='command', required=True)
     build_parser = command_parser.add_parser('build', help='build targets')
     build_parser.add_argument('target', help='[llvm|llvm-mcheck|haskell]', type=BuildTarget)
+
+    run_parser = command_parser.add_parser('run', help='run programs')
+    run_parser.add_argument('input_file', help='Input MediK File', type=file_path)
     return parser
 
 
@@ -80,4 +42,7 @@ def main() -> None:
     parser = _build_arg_parser()
     args = parser.parse_args()
     match args.command:
-        case 'build': args.target.do_build()
+        case 'build':
+            args.target.do_build()
+        case 'run':
+            exec_run(args.input_file)
