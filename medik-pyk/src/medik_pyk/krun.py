4a2fad6a5e6bd356d9aa9f1b3f0b0af7a65c65ac
Manasvi Saxena <msaxena2@illinois.edu>
2023-10-04 18:08:03 -0500

    >> WIP on implementing krun+medik

--- medik-pyk/src/medik_pyk/krun.py
+++ medik-pyk/src/medik_pyk/krun.py
@@ -1,17 +1,25 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
-from pyk.ktool.krun import KRunOutput
+from pyk.ktool.krun import KRunOutput, _krun
 
 from .build import BuildTarget
 
 if TYPE_CHECKING:
     from pathlib import Path
-    from typing import Any
+    from typing import Any, Final
+
+_LOGGER: Final = logging.getLogger(__name__)
+_LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
 
 
 def medik_run(
     input_file: Path, target: BuildTarget = BuildTarget.LLVM, output_mode: KRunOutput = KRunOutput.PRETTY, **kwargs: Any
 ) -> None:
-    pass
+    if not target.path.exists():
+        raise RuntimeError(f'Target {target.value} not built. Build target before execution.')
+
+
+
--- medik-pyk/src/medik_pyk/medik.py
+++ medik-pyk/src/medik_pyk/medik.py
@@ -8,10 +8,11 @@ from pyk.cli.utils import file_path
 from pyk.ktool.krun import KRunOutput
 
 from .build import BuildTarget
+from .krun import medik_run
 
 if TYPE_CHECKING:
     from pathlib import Path
-    from typing import Final
+    from typing import Any, Final
 
 
 _LOGGER: Final = logging.getLogger(__name__)
@@ -22,9 +23,28 @@ _LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
 # ----------------
 
 
-def exec_run(input_file: Path, output_mode: KRunOutput = KRunOutput.PRETTY) -> None:
-    if not BuildTarget.LLVM.path.exists():
-        raise RuntimeError(f'Target {str(BuildTarget.LLVM.value)} does not exist.')
+def exec_run(
+    input_file: Path,
+    do_search: bool = False,
+    output_mode: KRunOutput = KRunOutput.PRETTY,
+    search_pattern: str | None = None,
+    depth: int | None = None,
+    **kwargs: Any,
+) -> None:
+    if do_search:
+        target = BuildTarget.LLVM_MCHECK
+    else:
+        target = BuildTarget.LLVM
+
+    medik_run(
+        input_file=input_file,
+        target=target,
+        output_mode=output_mode,
+        do_search=do_search,
+        search_pattern=search_pattern,
+        depth=depth,
+        **kwargs,
+    )
 
 
 def _build_arg_parser() -> ArgumentParser:
@@ -35,6 +55,17 @@ def _build_arg_parser() -> ArgumentParser:
 
     run_parser = command_parser.add_parser('run', help='run programs')
     run_parser.add_argument('input_file', help='Input MediK File', type=file_path)
+    run_parser.add_argument(
+        '--output',
+        help='Medik Output Format [pretty|kore|json|none]',
+        type=KRunOutput,
+        default=KRunOutput.PRETTY,
+        dest='output_mode',
+    )
+    run_parser.add_argument('--search', help='Perform a search', action='store_true', dest='do_search')
+    run_parser.add_argument('--pattern', help='Pattern to search for', type=str, dest='search_pattern')
+    run_parser.add_argument('--depth', help='Execution Depth', type=int)
+
     return parser
 
 
@@ -45,4 +76,4 @@ def main() -> None:
         case 'build':
             args.target.do_build()
         case 'run':
-            exec_run(args.input_file)
+            exec_run(args.input_file, do_search=args.do_search, output_mode=args.output_mode)
